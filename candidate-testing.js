4f877c80fc67b11d26eb9f52c2d9dd4cc092c5ae
Kyle Banderman <kyle.banderman4675@gmail.com>
2023-09-25 19:47:17 -0500

    completed graded assignment 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,16 +9,14 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
-
 //TODO: Variables for Part 2
-let questions = ["Who was the first American Woman in space?  " , "True or false: 5 kilometer == 5000 meters?  " , "(5 + 3)/2 * 10 = ?  " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?  " , "What is the minimum crew size for the ISS?  " ];
+let questions = ["Who was the first American woman in space? " , "True or false: 5 kilometer == 5000 meters? " , "(5 + 3)/2 * 10 = ? " , "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? " , "What is the minimum crew size for the ISS? " ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
- let candidateName = input.question("What is your name?");
- console.log(candidateName);
+ candidateName = input.question("What is your name?");
 }
 
 function askQuestion() {
@@ -26,25 +24,29 @@ function askQuestion() {
   for (let i = 0; i < questions.length; i++) {
     candidateAnswers.push(input.question(questions[i]));
   }
-console.log(candidateAnswers);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
- console.log(candidateAnswers);
- console.log(correctAnswers);
- console.log(candidateName);
-  if (candidateAnswers === correctAnswers) {
-    console.log("Correct! Well Done!")
+ let grade = 0;
+ let innerGrade = 0;
+ for (let i = 0; i < correctAnswers.length; i++){
+  if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+    innerGrade++;
+    console.log(`Correct! Well Done! ${innerGrade} out of 5!`);
   } else {
-    console.log("Sorry, you got it wrong.")
+    console.log(`Sorry, you got it wrong. ${innerGrade} out of 5.`);
   }
+ }
 
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
+  //TODO 3.2 use this variable to calculate the candidates score.
+grade = (innerGrade / questions.length) * 100;
+if (grade >= 80) {
+  console.log("Congrats! You passed!");
+} else {
+  console.log("I'm sorry. You failed. Please try again.");
+}
   return grade;
 }
 
@@ -54,6 +56,10 @@ function runProgram() {
    console.log(`Hello ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
+  console.log(candidateAnswers);
+  console.log(correctAnswers);
+  console.log(candidateAnswers[0].toLowerCase() === correctAnswers[0].toLowerCase());
+  //if (candidateAnswers[0].to)
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
